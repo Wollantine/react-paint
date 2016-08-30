be5cf45b17907c52efe0979dea002d0f062b1b0f
Marc Rodriguez <marc.r@enabl.es>
2016-08-30 19:20:41 +0200

    Added undoer tests.

--- test/components/Undoer.test.js
+++ test/components/Undoer.test.js
@@ -1,24 +1,43 @@
+import React from 'react';
 import {render, mount} from 'enzyme';
-import UndoerView from '../../src/components/undoer/UndoerView.jsx';
+import sinon from 'sinon';
+import {UndoerContainer} from '../../src/components/undoer/Undoer.jsx';
 
-describe('<UndoerView/>', () => {
+describe('<Undoer/>', () => {
 
     let view = null,
-        wrapper = null;
+        wrapper = null,
+        onUndo = sinon.spy(),
+        onRedo = sinon.spy();
 
-    before(() => {
-        // view = UndoerView({brushSize, changeBrushSize});
-        // wrapper = render(view);
+    it('should render two buttons', () => {
+        view = <UndoerContainer onUndo={onUndo} onRedo={onRedo} past={['1']} future={['2']} />;
+        wrapper = render(view);
+        wrapper.should.have.exactly(2).descendants('button');
     })
 
-    it('should render two buttons')
+    it('should disable first button when past is empty', () => {
+        view = <UndoerContainer onUndo={onUndo} onRedo={onRedo} past={[]} future={['2']} />;
+        wrapper = render(view);
+        wrapper.find('button').first().should.be.disabled();
+    })
 
-    it('should disable first button when past is empty')
+    it('should enable first button when past is not empty', () => {
+        view = <UndoerContainer onUndo={onUndo} onRedo={onRedo} past={['1']} future={['2']} />;
+        wrapper = render(view);
+        wrapper.find('button').first().should.not.be.disabled();
+    })
 
-    it('should enable first button when past is not empty')
+    it('should disable second button when future is empty', () => {
+        view = <UndoerContainer onUndo={onUndo} onRedo={onRedo} past={['1']} future={[]} />;
+        wrapper = render(view);
+        wrapper.find('button').last().should.be.disabled();
+    })
 
-    it('should disable second button when future is empty')
-
-    it('should enable second button when future is not empty')
+    it('should enable second button when future is not empty', () => {
+        view = <UndoerContainer onUndo={onUndo} onRedo={onRedo} past={['1']} future={['2']} />;
+        wrapper = render(view);
+        wrapper.find('button').last().should.not.be.disabled();
+    })
 
 });
